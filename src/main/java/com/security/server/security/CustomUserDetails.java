555072375615ae2a4731cb9d59b985ca2f745335
AryanSoni27 <aryansoni33635@gmail.com>
2026-03-27 00:27:18 +0530

    implement CustomUserDetails

---
+++ src/main/java/com/security/server/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.security.server.security;
+
+import com.security.server.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user, Collection<? extends GrantedAuthority> authorities){
+        this.user = user;
+        this.authorities = authorities;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.getAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.getCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getEnabled();
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+}
---
+++ src/main/java/com/security/server/security/CustomUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.security.server.security;
+
+import com.security.server.entity.*;
+import com.security.server.repository.RolePermissionRepository;
+import com.security.server.repository.UserRepository;
+import com.security.server.repository.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final RolePermissionRepository rolePermissionRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameAndEmail(identifier, identifier)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        List<UserRole> userRoles = userRoleRepository.findByUser(user);
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (UserRole userRole : userRoles){
+            Role role = userRole.getRole();
+
+            authorities.add(new SimpleGrantedAuthority("Role_" + role.getRoleName()));
+
+            List<RolePermission> rolePermissions = rolePermissionRepository.findByRole(role);
+
+            for (RolePermission rp : rolePermissions){
+                Permission permission = rp.getPermission();
+                authorities.add(new SimpleGrantedAuthority(permission.getName()));
+            }
+        }
+        return new CustomUserDetails(user, authorities);
+    }
+}
